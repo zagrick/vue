e06f0bcc068fc7dbab339a381b41e8fffa6d4893
almazim89 <alimzagrick@gmail.com>
2020-03-03 10:31:39 +0300

    new

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum illo,
+      nesciunt minus consectetur aut mollitia autem voluptatum culpa ratione
+      aliquid, aspernatur ab recusandae voluptates at sunt corrupti ullam nisi
+      quae!
+    </p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
